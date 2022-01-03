b79026750511fda57a1df73642cf11165628a03b
John Lockwood <elitepropertiesbroker@gmail.com>
2022-01-03 09:47:22 -0500

    Shell script for day 5 to install pyenv

---
+++ day-05/configure-pyenv.sh
@@ -0,0 +1,4 @@
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zprofile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zprofile
+echo 'eval "$(pyenv init --path)"' >> ~/.zprofile
+echo 'eval "$(pyenv init -)"' >> ~/.zprofile
